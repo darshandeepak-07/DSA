289dbc814f5e1978e3c6b691c699ff8520168db9
deepak <darshandeepak1483@gmail.com>
2025-03-22 11:10:22 +0530

    implemented selection sort for desc order

--- sort/selection_sort.java
+++ sort/selection_sort.java
@@ -5,6 +5,7 @@ public class selection_sort {
         int nums[] = { 3, 1, 5, 6, 10, 12, 15, 2 };
         int minIndex = -1;
 
+        // ascending order
         for (int i = 0; i < nums.length - 1; i++) {
             minIndex = i;
 
@@ -23,5 +24,28 @@ public class selection_sort {
             System.out.print(nums[i] + ",");
         }
         System.out.println();
+
+        int maxIndex = -1;
+
+        // descending order
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxIndex = i;
+
+            for (int j = i +1 ; j < nums.length - i; j++) {
+                if (nums[maxIndex] < nums[j]) {
+                    maxIndex = j;
+                }
+            }
+
+            int temp = nums[maxIndex];
+            nums[maxIndex] = nums[i];
+            nums[i] = temp;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + ",");
+        }
+        System.out.println();
+
     }
 }
