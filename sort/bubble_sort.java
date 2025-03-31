16cd37381fe40f177ce0e9966e93a36a7b7108b7
deepak <darshandeepak1483@gmail.com>
2025-03-31 19:50:20 +0530

    implemented resursive solution for bubble sort and insertion sort

--- sort/bubble_sort.java
+++ sort/bubble_sort.java
@@ -1,8 +1,29 @@
 package sort;
 
 public class bubble_sort {
+
+    // using recursion
+
+    public static void bubbleSort(int arr[], int n) {
+        if (n == 1) return;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                arr[i] = arr[i] ^ arr[i + 1];
+                arr[i + 1] = arr[i] ^ arr[i + 1];
+                arr[i] = arr[i] ^ arr[i + 1];
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+        bubbleSort(arr, n - 1);
+    }
+
     public static void main(String[] args) {
-        int nums[] = { 2, 1, 5, 7, 4, 8, 6, 3, 10 };
+        int nums[] = {2, 1, 5, 7, 4, 8, 6, 3, 10};
         int size = nums.length - 1;
         int temp = 0;
 
@@ -25,5 +46,7 @@ public class bubble_sort {
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + ",");
         }
+
+        bubbleSort(nums, nums.length);
     }
 }
--- sort/insertion_sort.java
+++ sort/insertion_sort.java
@@ -2,8 +2,29 @@ package sort;
 
 public class insertion_sort {
 
+    // recursively sort using insertion sort
+    public static void insertionSort(int nums[], int i, int n) {
+        if (i == n) return;
+
+        int key = nums[i];
+        int j = i - 1;
+
+        while (j >= 0 && nums[j] > key) {
+            nums[j + 1] = nums[j];
+            j--;
+        }
+
+        nums[j + 1] = key;
+
+        for (int k = 0; k < nums.length; k++) {
+            System.out.print(nums[k]);
+        }
+        System.out.println();
+        insertionSort(nums, i + 1, n);
+    }
+
     public static void main(String[] args) {
-        int nums[] = { 1, 4, 2, 7, 3, 10, 5 };
+        int nums[] = {1, 4, 2, 7, 3, 10, 5};
 
         for (int i = 1; i < nums.length; i++) {
             int key = nums[i];
@@ -19,5 +40,7 @@ public class insertion_sort {
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + ",");
         }
+
+        insertionSort(nums, 0, nums.length);
     }
 }
