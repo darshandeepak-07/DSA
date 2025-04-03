979eb29a07d719f4a91b3aac9b4b985f01980e00
deepak <darshandeepak1483@gmail.com>
2025-04-03 21:20:57 +0530

    done with 4 easy problems

--- array/Easy.java
+++ array/Easy.java
@@ -1,6 +1,9 @@
 package array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Easy {
 
@@ -61,6 +64,60 @@ public class Easy {
             }
         }
         return true;
+    }
+
+    //Problem 4
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int temp[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
+        }
+    }
+
+    //Problem 5
+    public void moveZeroes(int[] nums) {
+        int l = 0;
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] != 0) {
+                int temp = nums[r];
+                nums[r] = nums[l];
+                nums[l] = temp;
+                l++;
+            }
+        }
+    }
+
+    // Problem 6 - To find union of two array in sorted order
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        Set<Integer> set = new TreeSet<>();
+
+        for (int num : a) {
+            set.add(num);
+        }
+
+        for (int num : b) {
+            set.add(num);
+        }
+
+        return new ArrayList<>(set);
+
+    }
+
+    //Problem 7 - To find missing number
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        return expectedSum - sum;
     }
 
     public static void main(String[] args) {
