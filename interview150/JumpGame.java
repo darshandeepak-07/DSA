00bc9a2ed3be7f865dcb3a9c71306430de2972cc
deepak <darshandeepak1483@gmail.com>
2025-05-04 20:49:03 +0530

    jump game 2

--- interview150/JumpGame.java
+++ interview150/JumpGame.java
@@ -2,6 +2,7 @@ package interview150;
 
 public class JumpGame {
 
+    // Level 1
     public static boolean canJump(int[] nums) {
         int reachableIndex = 0;
 
@@ -13,8 +14,26 @@ public class JumpGame {
         return true;
     }
 
+    // Level 2
+    public static int jump(int[] nums) {
+        int res = 0, l = 0, r = 0;
+
+        while (r < nums.length - 1) {
+            int farthest = 0;
+
+            for (int i = l; i <= r; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+
+            l = r + 1;
+            r = farthest;
+            res++;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] nums = {2, 3, 1, 1, 4};
-        System.out.println(canJump(nums));
+        System.out.println(jump(nums));
     }
 }
