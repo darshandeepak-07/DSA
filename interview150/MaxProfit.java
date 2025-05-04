2c7317e04cae220f937f70ebde93099514add2c2
deepak <darshandeepak1483@gmail.com>
2025-05-04 18:35:39 +0530

    BTTBS 2

--- interview150/MaxProfit.java
+++ interview150/MaxProfit.java
@@ -2,29 +2,38 @@ package interview150;
 
 public class MaxProfit {
 
+    // Level 1
     public static int maxProfit(int[] prices) {
-        int min = prices[0], max = 0;
-        int minIndex = 0;
-        for (int i = 0; i < prices.length - 1; i++) {
-            if (prices[i] < min) {
-                min = prices[i];
-                minIndex = i;
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
             }
         }
 
-        for (int i = minIndex + 1; i < prices.length; i++) {
-            if (prices[i] > max) max = prices[i];
-        }
-        int profit = 0;
-        int res = max - min;
-        if (res > 0) {
-            profit = res;
-        }
-        return profit;
+        return maxProfit;
     }
 
+    // Level 2
+    public static int maxProfit2(int[] prices) {
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return maxProfit;
+    }
+
+
     public static void main(String[] args) {
-        int[] nums = {7, 6, 4, 3, 1};
-        System.out.println(maxProfit(nums));
+        int[] nums = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit2(nums));
     }
 }
